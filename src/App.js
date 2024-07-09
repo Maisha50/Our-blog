95cb61b48149525ed523ea1cb735015b04f625b0
Maisha50 <mihamaisha205@gmail.com>
2024-07-10 00:45:07 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import {Main} from './components/Main';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Screens/Home';
+import BlogDetails from './Screens/BlogDetails';
+import Login from './Screens/Login';
+import Signup from './Screens/Signup';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <Router>
+    <div >
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/blog/:id' element={<BlogDetails/>}/>
+        <Route exact path='/login' element={<Login/>}/>
+        <Route exact path='/signup' element={<Signup/>}/>
+
+        
+        
+      </Routes>
+      
+      
     </div>
+    </Router>
   );
 }
 
