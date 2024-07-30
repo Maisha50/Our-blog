4027e8738209164583f3a953d731ffc9c4251250
Maisha50 <mihamaisha205@gmail.com>
2024-07-31 00:51:39 +0600

    post-comment

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -3,19 +3,18 @@ import { Grid } from '@mui/material';
 // Components
 import Banner from '../banner/Banner';
 import Categories from '../home/categories';
-
-
+import Posts from './post/Posts';
 
 const Home = () => {
     return (
         <>
-            { <Banner /> }
+            <Banner />
             <Grid container spacing={2}>
                 <Grid item lg={2} xs={12} sm={2}>
                     <Categories />
                 </Grid>
                 <Grid item xs={12} sm={10} lg={10}>
-                    
+                    <Posts /> {/* Add Posts component here */}
                 </Grid>
             </Grid>
         </>
