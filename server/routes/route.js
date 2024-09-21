91eb0376def329c2f4686017a8ff6b94036b81da
Maisha50 <mihamaisha205@gmail.com>
2024-09-21 16:22:11 +0600

    as

--- server/routes/route.js
+++ server/routes/route.js
@@ -20,17 +20,17 @@ router.post('/profile', Profile);
 router.post('/token', createNewToken);
 
 router.post('/create', createPost);
-router.put('/update/:id', authenticateToken, updatePost);
-router.delete('/delete/:id', authenticateToken, deletePost);
+router.put('/update/:id',  updatePost);
+router.delete('/delete/:id', deletePost);
 
-router.get('/post/:id', authenticateToken, getPost);
-router.get('/posts', authenticateToken, getAllPosts);
+router.get('/post/:id', getPost);
+router.get('/posts', getAllPosts);
 
 router.post('/file/upload', upload.single('file'), uploadImage);
 router.get('/file/:filename', getImage);
 
-router.post('/comment/new', authenticateToken, newComment);
-router.get('/comments/:id', authenticateToken, getComments);
-router.delete('/comment/delete/:id', authenticateToken, deleteComment);
+router.post('/comment/new', newComment);
+router.get('/comments/:id',  getComments);
+router.delete('/comment/delete/:id', deleteComment);
 router.post('/dashboard', dashboard);
 export default router;
